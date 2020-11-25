e5fcb6a9c6ba11323c9c1f2d178318b0410d55e8
danielrussellla <danielrussell16@gmail.com>
2020-11-25 10:51:00 -0800

    got colors working the way i want them

--- src/components/colorCanvas.js
+++ src/components/colorCanvas.js
@@ -1,72 +1,74 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import canvasSketch from 'canvas-sketch';
 import { lerp } from 'canvas-sketch-util/math'
 import random from 'canvas-sketch-util/random'
 import palettes from 'nice-color-palettes'
 import { css } from '@emotion/core'
-// const canvasSketch = require('canvas-sketch');
-// const { lerp } = require('canvas-sketch-util/math');
-// const random = require('canvas-sketch-util/random');
-// const palettes = require('nice-color-palettes');
 
-const sketch = () => {
-    const colorCount = random.rangeFloor(12, 24);
-    const palette = random.shuffle(random.pick(palettes))
-        .slice(0, colorCount);
 
-    const createGrid = () => {
-        const points = [];
-        const count = 53;
-        for (let x = 0; x < count; x++) {
-            for (let y = 0; y < count; y++) {
-                const u = count <= 1 ? 0.5 : x / (count - 1);
-                const v = count <= 1 ? 0.5 : y / (count - 1);
-                points.push({
-                    color: random.pick(palette),
-                    radius: Math.abs(0.01 + random.gaussian() * 0.01),
-                    position: [u, v]
-                });
+const ColorCanvas = ({ id, show, duration }) => {
+    let [rendered, setRendered] = useState(false)
+
+    useEffect(() => {
+        let colorCount = random.rangeFloor(6, 24)
+        console.log("COLOR_COUNT", colorCount)
+        let palette = random.shuffle(random.pick(palettes))
+            .slice(0, colorCount)
+        const createGrid = () => {
+            const points = [];
+            const count = 53;
+            for (let x = 0; x < count; x++) {
+                for (let y = 0; y < count; y++) {
+                    const u = count <= 1 ? 0.5 : x / (count - 1);
+                    const v = count <= 1 ? 0.5 : y / (count - 1);
+                    points.push({
+                        color: random.pick(palette),
+                        radius: Math.abs(0.01 + random.gaussian() * 0.01),
+                        position: [u, v]
+                    });
+                }
+            }
+            return points;
+        }
+        const sketch = () => {
+            random.setSeed(512)
+            const points = createGrid().filter(() => random.value() > 0.5 ? true : false)
+            const margin = 0;
+            // render fn
+            return ({ context, width, height }) => {
+
+                context.fillStyle = '#333';
+                context.fillRect(0, 0, width, height);
+
+                points.forEach((point) => {
+                    const { position, radius, color } = point;
+                    const [u, v] = position;
+                    const x = lerp(margin, width - margin, u);
+                    const y = lerp(margin, width - margin, v);
+
+                    context.beginPath();
+                    context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+                    context.fillStyle = color;
+                    context.fill();
+                })
             }
         }
-        return points;
-    }
-
-    random.setSeed(512)
-    const points = createGrid().filter(() => random.value() > 0.5 ? true : false)
-    const margin = 0;
-    // render fn
-    return ({ context, width, height }) => {
-
-        context.fillStyle = '#333';
-        context.fillRect(0, 0, width, height);
-
-        points.forEach((point) => {
-            const { position, radius, color } = point;
-            const [u, v] = position;
-            const x = lerp(margin, width - margin, u);
-            const y = lerp(margin, width - margin, v);
-
-            context.beginPath();
-            context.arc(x, y, radius * width, 0, Math.PI * 2, false);
-            context.fillStyle = color;
-            context.fill();
-        })
-    }
-}
-
-const ColorCanvas = () => {
-    useEffect(() => {
         const CANVAS_SETTINGS = {
-            // scaleToFit: true,
             scaleContext: true,
-            parent: document.getElementById('canvas')
+            parent: document.getElementById(id)
         }
-        canvasSketch(sketch, CANVAS_SETTINGS);
-    })
+        if (!rendered) {
+            canvasSketch(sketch, CANVAS_SETTINGS);
+            setRendered(true)
+        }
+    }, [id, show, rendered])
+
     return (
-        <div id="canvas" css={css`
+        <div id={id} css={css`
             height: 100%;
             min-width: 1000px;
+            opacity: ${show ? '1' : '0'};
+            transition: opacity ${duration}s ease-in-out;
             @media (max-width: 1000px) {
                 margin-left: -6rem;
             }
--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { css } from '@emotion/core'
 
+import TitleCard from './titleCard'
 import ColorCanvas from './colorCanvas'
 
+const ANIMATION_DURATION = 6 // seconds
+const NUM_PERMUTATIONS = 30
+let CURR_IDX = 0
+
 const Header = () => {
+    let [toggles, setToggles] = useState(new Array(NUM_PERMUTATIONS).fill(0))
+
+    let [start, setStart] = useState(false)
+
+    const advanceColors = (toggles) => {
+        let newToggles = [...toggles]
+        if (CURR_IDX === newToggles.length) CURR_IDX = 0
+        newToggles.forEach((toggle, idx) => newToggles[idx] = 0)
+        newToggles[CURR_IDX] = 1
+        setToggles(newToggles)
+        CURR_IDX = CURR_IDX + 1
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            advanceColors(toggles)
+        }, ANIMATION_DURATION * 1000)
+        if (!start) {
+            advanceColors(toggles)
+            setStart(true)
+        }
+    }, [toggles, start])
+
     return (
         <header css={css`
             height: 100vh;
             position: relative;
             overflow: hidden;
         `}>
+            {toggles.map((state, idx) => {
+                return (
+                    <div
+                        key={idx}
+                        css={css`
+                            height: 100%;
+                            width: 100%;
+                            position: absolute;
+                            top: 0;
+                            left: 0;
+                        `}
+                    >
+                        <ColorCanvas
+                            id={`canvas-${idx}`}
+                            show={state}
+                            duration={ANIMATION_DURATION}
+                        />
+                    </div>
+                )
+            })}
             <div css={css`
                 position: absolute;
                 top: 0;
@@ -26,40 +74,9 @@ const Header = () => {
                     margin: -6rem 0 0;
                 }
             `}>
-                <div css={css`
-                    background: #151515;
-                    text-align: center;
-                    padding: 1rem;
-                    border-radius: 8px;
-                    border: 4px solid #fff;
-                    padding: 2rem;
-                    box-shadow: 8px 8px 4px rgba(0,0,0,.5);
-                    h1 {
-                        color: white;
-                        margin: 0;
-                        font-weight: normal;
-                        font-family: 'Courier New', Courier, monospace;
-                        font-size: min(max(1rem,4vw),1.6rem);
-                    }
-                    h1:first-of-type {
-                        margin-bottom: 0.75rem;
-                    }
-                    p {
-                        color: #fff;
-                        margin: 0;
-                    }
-                    b {
-                        color: #fff;
-                        font-weight: 600;
-                    }
-                `}>
-                    <h1>
-                        Hello, I'm <b>Daniel Russell</b>.
-                    </h1>
-                    <h1>I'm a <b>fullstack web developer</b>.</h1>
-                </div>
+                <TitleCard />
             </div>
-            <ColorCanvas />
+
         </header>
     )
 }
